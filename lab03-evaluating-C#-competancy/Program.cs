c8e58d13107d7e8a852508beb82d496a109cd4c3
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-06-19 12:37:12 +0300

    Add project files.

---
+++ lab03-evaluating-C#-competancy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab03_evaluating_C__competancy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
